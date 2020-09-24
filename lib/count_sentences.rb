3b330004dec2663b14b519e18833522ccf24e7c8
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-24 15:10:22 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
-
+  element = self.split(/[.?!]/) 
+  element.delete_if{|e| e.length == 0}
+  element.count
   end
 end
